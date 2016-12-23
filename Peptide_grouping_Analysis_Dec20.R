94e80d262a2e440ae7459c8d60ca9dfac8e593b4
adiamb <ambati@stanford.edu>
2016-12-23 15:21:54 -0800

    commit@321pm

--- Peptide_grouping_Analysis_Dec20.R
+++ Peptide_grouping_Analysis_Dec20.R
@@ -25,6 +25,23 @@ flu_main$subtype=ifelse(str_detect(flu_main$FULLNAME, "California"), "pH1N1",
               ifelse(str_detect(flu_main$FULLNAME, "Puerto|South"), "pastpH1N1",
                      "H3N2")))
 
+
+
+flu_main$Protname = ifelse(str_detect(flu_main$FULLNAME, "Polymerase basic protein 2|polymerase PB2"), "PB2",
+                           ifelse(str_detect(flu_main$FULLNAME, "Polymerase acidic protein|polymerase PA"), "PA",
+                                  ifelse(str_detect(flu_main$FULLNAME, "RNA-directed RNA polymerase catalytic subunit|polymerase PB1"), "PB1",
+                                         ifelse(str_detect(flu_main$FULLNAME, "Hemagglutinin|hemagglutinin|HA|haemagglutinin"), "HA",
+                                                ifelse(str_detect(flu_main$FULLNAME, "Neuraminidase|neuraminidase|NA"), "Neu",
+                                                       ifelse(str_detect(flu_main$FULLNAME, "Matrix protein 2|matrix protein 2"), "M2",
+                                                              ifelse(str_detect(flu_main$FULLNAME, "Matrix protein 1|matrix protein 1"),"M1",
+                                                                     ifelse(str_detect(flu_main$FULLNAME, "Nuclear export protein|nuclear export protein"),"NEP",
+                                                                            ifelse(str_detect(flu_main$FULLNAME, "Non-structural protein 1|non-structural protein 1|nonstructural protein 1"), "NS1",
+                                                                                   ifelse(str_detect(flu_main$FULLNAME, "Nucleoprotein|nucleocapsid protein|nucleoprotein"), "NP",
+                                                                                          ifelse(str_detect(flu_main$FULLNAME, "PA-X protein|PB1-F2"), "otherPA_PB",
+                                                                                                 ifelse(str_detect(flu_main$FULLNAME, "nonstructural protein 2"), "NS2",
+                                                                                                 NA))))))))))))
+                                                                                            
+
 flu_main_unique=flu_main[, list(PROTID = paste(unique(PROTID), collapse = "/"), FULLNAME = paste(unique(FULLNAME), collapse = "/")), by=list(SEQUENCE, subtype)]
 final_grouping=merge.data.frame(peplist2, flu_main_unique, by.x ="PEPTIDE_SEQUENCE", by.y = "SEQUENCE", all= T)
 
